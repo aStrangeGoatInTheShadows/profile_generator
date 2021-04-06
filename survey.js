ddfbe6c59fc9e4fa081e0cdb8d0315121178b36d
Matt Macdonald <mksmacdonald@gmail.com>
2021-04-06 19:08:10 +0000

    Attempting a different strategy as the last one was going nowhere"

--- survey.js
+++ survey.js
@@ -1,83 +1,25 @@
-const readline = require('readline');
-const profile = {};
+var readline = require('readline'),
+  rl = readline.createInterface(process.stdin, process.stdout),
+  prefix = 'OHAI> ';
 
-
-const inpootOutpoot = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+rl.on('line', function(line) {
+  switch(line.trim()) {
+    case 'hello':
+      console.log('world!');
+      break;
+    case 'close': console.log('bye bye');
+      rl.close();
+      break;
+    default:
+      console.log('Say what? I might have heard `' + line.trim() + '`');
+      break;
+  }
+  rl.setPrompt(prefix, prefix.length);
+  rl.prompt();
+}).on('close', function() {
+  console.log('Have a great day!');
+  process.exit(0);
 });
-
-const userQuery = (question, key) => {
-  
-  inpootOutpoot.question(question, (userIn) => {    
-    profile[key] = userIn;
-
-    console.log(`${key} is set to : ${profile[key]}`);
-    inpootOutpoot.pause();
-  });  
-};
-
-userQuery('What would youi like to name your profile?', 'name');
-userQuery('What\'s an activity you enjoy?', 'hobby');
-
-// rl.question('What would you like to name your profile?? ', (userIn) => {
-//   console.log(`Thanks: ${userIn}`);
-//   profile.name = userIn;
-
-//   rl.close();
-// });
-
-
-
-/*r1.question('What would you like to name your profile? ', (nmIn) => {
-  console.log(`Awesome choice: ${nmIn}`);
-
-  profile.name = nmIn;
-
-  r1.close();
-});*/
-
-console.log(profile);
-
-// What's your name? Nicknames are also acceptable :)
-// What's an activity you like doing?
-// What do you listen to while doing that?
-// Which meal is your favourite (eg: dinner, brunch, etc.)
-// What's your favourite thing to eat for that meal?
-// Which sport is your absolute favourite?
-// What is your superpower? In a few words, tell us what you are amazing at!
-
-'use strict'
-
-const readline = require('readline')
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
-
-const question1 = () => {
-  return new Promise((resolve, reject) => {
-    rl.question('q1 What do you think of Node.js? ', (answer) => {
-      console.log(`Thank you for your valuable feedback: ${answer}`)
-      resolve()
-    })
-  })
-}
-
-const question2 = () => {
-  return new Promise((resolve, reject) => {
-    rl.question('q2 What do you think of Node.js? ', (answer) => {
-      console.log(`Thank you for your valuable feedback: ${answer}`)
-      resolve()
-    })
-  })
-}
-
-const main = async () => {
-  await question1()
-  await question2()
-  rl.close()
-}
-
-main()
+console.log(prefix + 'Good to see you. Try typing stuff.');
+rl.setPrompt(prefix, prefix.length);
+rl.prompt();
