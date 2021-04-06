8d2be85b72fc18f85bffde0fa873537dc72cf6b7
Matt Macdonald <mksmacdonald@gmail.com>
2021-04-06 19:17:04 +0000

    starting to build and alter logic flow

--- survey.js
+++ survey.js
@@ -1,9 +1,24 @@
 var readline = require('readline'),
   rl = readline.createInterface(process.stdin, process.stdout),
-  prefix = 'OHAI> ';
+  prefix = '> ';
 
-rl.on('line', function(line) {
-  switch(line.trim()) {
+  
+  const setupQuestions = [
+    "What's your name? Nicknames are also acceptable",
+    "What's an activity you like doing?",
+    "What do you listen to while doing that?",
+    "Which meal is your favourite (eg: dinner, brunch, etc.)",
+    "Which meal is your favourite (eg: dinner, brunch, etc.)",
+    "Which sport is your absolute favourite?",
+    "What is your superpower? In a few words, tell us what you are amazing at!"
+  ]
+ 
+  const profile = {
+    init: setupQuestions
+  }
+
+rl.on('line', function (line) {
+  switch (line.trim()) {
     case 'hello':
       console.log('world!');
       break;
@@ -16,10 +31,20 @@ rl.on('line', function(line) {
   }
   rl.setPrompt(prefix, prefix.length);
   rl.prompt();
-}).on('close', function() {
+}).on('close', function () {
   console.log('Have a great day!');
   process.exit(0);
 });
-console.log(prefix + 'Good to see you. Try typing stuff.');
-rl.setPrompt(prefix, prefix.length);
+
+console.log(prefix + 'Please setup your profile.');
+rl.setPrompt(profile.init[0], profile.init[0].length);
 rl.prompt();
+
+// What's your name? Nicknames are also acceptable :)
+// 
+// 
+// 
+// Which sport is your absolute favourite?
+// W
+
+
