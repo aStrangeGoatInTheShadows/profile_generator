2f18a2209ea3dc6dc8202fc5b1ee51251c8e3642
Matt Macdonald <mksmacdonald@gmail.com>
2021-04-06 19:37:46 +0000

    logic working for flow, just need to check stored data

--- survey.js
+++ survey.js
@@ -4,32 +4,52 @@ var readline = require('readline'),
 
   
   const setupQuestions = [
-    "What's your name? Nicknames are also acceptable",
+    "What's your name? Nicknames are also acceptable?",
     "What's an activity you like doing?",
     "What do you listen to while doing that?",
     "Which meal is your favourite (eg: dinner, brunch, etc.)",
-    "Which meal is your favourite (eg: dinner, brunch, etc.)",
+    "What's your favourite thing to eat for that meal?",
     "Which sport is your absolute favourite?",
     "What is your superpower? In a few words, tell us what you are amazing at!"
   ]
+
+  const setupKeys = [
+    'name',
+    'hobby',
+    'music',
+    'meal',
+    'food',
+    'sport',
+    'superPower'
+  ]
  
   const profile = {
-    init: setupQuestions
+    initQ: setupQuestions,
+    intiK: setupKeys,
+    initI: 0 // index to step through setup
   }
 
+  const p = profile;
+
 rl.on('line', function (line) {
+  
   switch (line.trim()) {
     case 'hello':
-      console.log('world!');
+      console.log('world! \n Seriously though.');
       break;
     case 'close': console.log('bye bye');
       rl.close();
       break;
     default:
-      console.log('Say what? I might have heard `' + line.trim() + '`');
+      p[p.intiK[p.initI]] = line;
+      console.log('');
+      p.initI++;
+      if(p.initI === p.initQ.length) {
+        rl.close();
+      }
       break;
   }
-  rl.setPrompt(prefix, prefix.length);
+  rl.setPrompt(p.initQ[p.initI], p.initQ[p.initI].length);
   rl.prompt();
 }).on('close', function () {
   console.log('Have a great day!');
@@ -37,14 +57,8 @@ rl.on('line', function (line) {
 });
 
 console.log(prefix + 'Please setup your profile.');
-rl.setPrompt(profile.init[0], profile.init[0].length);
+rl.setPrompt(p.initQ[p.initI], p.initQ[p.initI].length);
+p.InitI++;
 rl.prompt();
 
-// What's your name? Nicknames are also acceptable :)
-// 
-// 
-// 
-// Which sport is your absolute favourite?
-// W
-
-
+//console.log(p);
